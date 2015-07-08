d11183390e7a2804df0c2366f7a2c2bf9671d525
mobinityfx <mobinity.fx@gmail.com>
2015-07-08 20:57:26 +0700

    new database

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2015 at 10:00 PM
+-- Generation Time: Jul 08, 2015 at 03:52 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -40,16 +40,17 @@ CREATE TABLE IF NOT EXISTS `barang1` (
   `harga` int(255) NOT NULL,
   `gambar` varchar(100) NOT NULL,
   `deskripsi` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang1`
 --
 
 INSERT INTO `barang1` (`id`, `kategori`, `nama_barang`, `s`, `m`, `l`, `xl`, `ukuran`, `stok`, `berat`, `harga`, `gambar`, `deskripsi`) VALUES
-(14, 2, 'Jersey Juventus Second', 3, 1, 3, 3, 0, 0, 20, 10000, 'gbr_20150617203658.jpg', 'sdasfdsf'),
-(15, 1, 'MU Baru 2015', 2, 2, 1, 2, 0, 0, 3, 10000, 'gbr_20150617205510.jpg', 'Jersey MU'),
-(16, 3, 'Sepatu Adidas', 0, -2, 0, 0, 45, 3, 2, 200000, 'gbr_20150617205700.jpg', 'adidas');
+(14, 1, 'Jersey Juventus Second', 3, 3, 27, 11, 0, 0, 20, 10000, 'gbr_20150621175558.jpg', 'sdasfdsfxcv'),
+(16, 3, 'Sepatu Adidas', 0, 4, 25, 0, 32, 9, 2, 200000, 'gbr_20150621175025.jpg', 'adidas'),
+(18, 2, ' b', 1, 3, 26, 1, 0, 0, 2, 23, 'gbr_20150618205816.jpg', 'b'),
+(19, 2, 'Manchaster United', 3, 6, 28, 3, 0, 0, 2, 100000, 'gbr_20150622133702.jpg', 'Manchaster United');
 
 -- --------------------------------------------------------
 
@@ -63,19 +64,37 @@ CREATE TABLE IF NOT EXISTS `invoices` (
   `user_id` int(10) NOT NULL,
   `status` enum('paid','unconfirmed','confirmed','unpaid','pengiriman','batal') NOT NULL,
   `bukti` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `invoices`
 --
 
 INSERT INTO `invoices` (`id`, `date`, `user_id`, `status`, `bukti`) VALUES
-(7, '2015-06-17 00:00:00', 2, 'paid', ''),
-(8, '2015-06-17 00:00:00', 2, 'unconfirmed', ''),
-(9, '2015-06-17 00:00:00', 2, 'unconfirmed', ''),
-(10, '2015-06-17 00:00:00', 2, 'confirmed', ''),
-(11, '2015-06-17 00:00:00', 2, 'unconfirmed', ''),
-(12, '2015-06-17 00:00:00', 4, 'unconfirmed', '');
+(7, '2015-06-17 00:00:00', 2, 'batal', ''),
+(8, '2015-06-17 00:00:00', 2, 'batal', ''),
+(9, '2015-06-17 00:00:00', 2, 'batal', ''),
+(10, '2015-06-17 00:00:00', 2, 'batal', ''),
+(11, '2015-06-17 00:00:00', 2, 'batal', ''),
+(12, '2015-06-17 00:00:00', 4, 'batal', ''),
+(13, '2015-06-17 00:00:00', 4, 'batal', ''),
+(14, '2015-06-18 00:00:00', 5, 'paid', ''),
+(15, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(16, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(17, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(18, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(19, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(20, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(21, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(22, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(23, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(24, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(25, '2015-06-29 00:00:00', 5, 'unconfirmed', ''),
+(26, '2015-06-29 00:00:00', 5, 'batal', ''),
+(27, '2015-06-29 00:00:00', 5, 'batal', ''),
+(28, '2015-06-29 00:00:00', 5, 'batal', ''),
+(29, '2015-06-29 00:00:00', 5, 'batal', ''),
+(30, '2015-06-29 00:00:00', 5, 'batal', '');
 
 -- --------------------------------------------------------
 
@@ -114,6 +133,13 @@ CREATE TABLE IF NOT EXISTS `konfirm_pembayaran` (
   `scan_struk` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `konfirm_pembayaran`
+--
+
+INSERT INTO `konfirm_pembayaran` (`id_invoice`, `id_user`, `dari_bank`, `nama_pemilik`, `tujuan_transfer`, `jumlah_transfer`, `tanggal`, `scan_struk`) VALUES
+(14, 5, 'Mandiri', 'Retno', 'Mandiri', 30000, '2015-06-24', 'bukti_20150624180831.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -130,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `options` text NOT NULL,
   `pengiriman` int(11) NOT NULL,
   `total_harga` int(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
@@ -140,7 +166,15 @@ INSERT INTO `orders` (`id`, `invoice_id`, `product_id`, `product_name`, `qty`, `
 (7, 7, 13, 'Baju Second Bola', 2, 20000, 'm', 2, 1),
 (8, 10, 14, 'Jersey Juventus Second', 2, 20000, 'm', 1, 1),
 (9, 11, 15, 'MU Baru 2015', 1, 10000, 'l', 0, 0),
-(10, 12, 16, 'Sepatu Adidas', 2, 400000, 'm', 0, 0);
+(10, 12, 16, 'Sepatu Adidas', 2, 400000, 'm', 222222, 400222),
+(11, 13, 14, 'Jersey Juventus Second', 3, 30000, 'l', 0, 0),
+(12, 14, 15, 'MU Baru 2015', 2, 20000, 'L', 30000, 50000),
+(13, 25, 16, 'Sepatu Adidas', 3, 600000, '3', 0, 0),
+(14, 26, 16, 'Sepatu Adidas', 3, 600000, '3', 0, 0),
+(15, 27, 16, 'Sepatu Adidas', 3, 600000, '3', 0, 0),
+(16, 28, 14, 'Jersey Juventus Second', 20, 200000, 'l', 0, 0),
+(17, 29, 16, 'Sepatu Adidas', 3, 600000, '3', 0, 0),
+(18, 30, 19, 'Manchaster United', 2, 200000, 'l', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -195,7 +229,7 @@ CREATE TABLE IF NOT EXISTS `testimoni` (
   `nama` varchar(30) NOT NULL,
   `kota` varchar(30) NOT NULL,
   `testimoni` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `testimoni`
@@ -205,7 +239,8 @@ INSERT INTO `testimoni` (`id`, `nama`, `kota`, `testimoni`) VALUES
 (1, 'asdad', 'asdczxczxczc', 'adfvaef adfvgaref evfaregavre'),
 (2, 'Danur Wijayanto', 'Solo', 'Sanga bagus dan bla bla bla'),
 (3, 'zxcvcv', 'gdhdgjhgj', 'dghmhgkliuoyufy'),
-(4, 'danur', 'wija', 'bla asdfla;skdf a oapefk');
+(4, 'danur', 'wija', 'bla asdfla;skdf a oapefk'),
+(5, 'retno', 'Semarang', 'Oke lah');
 
 -- --------------------------------------------------------
 
@@ -224,16 +259,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `kecamatan` varchar(100) NOT NULL,
   `kelurahan` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `no_hp`, `alamat`, `provinsi`, `kota`, `kecamatan`, `kelurahan`, `password`) VALUES
-(2, 'danur wijayanto00', 'mobinity.fx@gmail.com', '085642180514', 'benowo rt 06 rw 08 ngringoooo', 'Jawa Tengah', 'Surakartaaa', 'Jaten', 'ngringo', '123123'),
-(3, 'danoer', 'widjayanto@gmail.com', '085642180514', 'semarang', 'q2ee', 'asdczxczxczc', 'zxcada', 'benowo', 'asdasdasd'),
-(4, 'Danur Wijayanto', 'danurwijayanto@gmail.com', '085642180514', 'benowo rt 06 rw 08 ngringo', 'Jawa Tengah', 'Surakarta', 'Jaten', 'Ngringo', 'nvidiagt440**//');
+(3, 'danoerr', 'widjayanto@gmail.com', '085642180514', 'semarang', 'q2ee', 'asdczxczxczc', 'jateng', 'benowo', 'asdasdasd'),
+(4, 'Danur Wijayanto', 'danurwijayanto@gmail.com', '085642180514', 'benowo rt 06 rw 08 ngringo', 'Jawa Tengah', 'Surakarta', 'Jaten', 'Ngringo', 'nvidiagt440**//'),
+(5, 'retno', 'retno@email.com', '1224546734', 'Jl. Jalan', 'Jawa Tengah', 'Semarang', 'Tembalang', 'Mangunharjo', 'satu1');
 
 --
 -- Indexes for dumped tables
@@ -301,12 +336,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `barang1`
 --
 ALTER TABLE `barang1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT for table `kategori1`
 --
@@ -316,7 +351,7 @@ ALTER TABLE `kategori1`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `petugas`
 --
@@ -326,12 +361,12 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT for table `testimoni`
 --
 ALTER TABLE `testimoni`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
